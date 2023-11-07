17ea9a04cac8dbd05e189c03ef8422cacc2b3f56
Oyeniyi Tolulope John <140219335+oyeniyi111@users.noreply.github.com>
2023-11-07 20:58:31 +0100

    Update home.js

--- js/home.js
+++ js/home.js
@@ -128,23 +128,6 @@ modalCloses.forEach((modalClose) =>{
 
 
 
- // accodian
- var acc = document.getElementsByClassName("accordion");
- var i;
- 
- for (i = 0; i < acc.length; i++) {
-   acc[i].addEventListener("click", function() {
-     this.classList.toggle("active");
-     var panel = this.nextElementSibling;
-     if (panel.style.display === "block") {
-       panel.style.display = "none";
-     } else {
-       panel.style.display = "block";
-     }
-   });
- }
-
-
      
 // progress bar
 $(document).ready(function(){
